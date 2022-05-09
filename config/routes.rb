be1b4124a02c936b19ab95e9ddd7fd67986cedef
imranprof <imran.ent320@gmail.com>
2022-05-09 19:02:34 +0600

    Setup tokens controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
 
+  namespace :api do
+    namespace :v1 do
+      get 'tokens/create'
+    end
+  end
   #Api definition
   namespace :api, defaults: {format: :json } do
     namespace :v1 do
       resources :users, only: [:show]
-
       resources :users, only: %i[:show create update destroy]
+
+      resources :tokens, only: [:create]
+
     end
   end
 
