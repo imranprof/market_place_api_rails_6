eb0433e429fbad1aedf73f981ab6fd42e02a57d8
imranprof <imran.ent320@gmail.com>
2022-05-09 20:10:48 +0600

    Finishes modeling the product model along with user associations

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json } do
     namespace :v1 do
       resources :users, only: [:show]
-      resources :users, only: %i[:show create update destroy]
+      resources :users, only: %i[show create update destroy]
 
       resources :tokens, only: [:create]
+
+      resources :products, only: %i[show index]
 
     end
   end
