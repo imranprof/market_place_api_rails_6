9a14c097782c6529ddeb657ce9c2d09a538132c2
imranprof <imran.ent320@gmail.com>
2022-05-09 20:38:11 +0600

    Adds the products create, update and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,12 @@ Rails.application.routes.draw do
   #Api definition
   namespace :api, defaults: {format: :json } do
     namespace :v1 do
-      resources :users, only: [:show]
       resources :users, only: %i[show create update destroy]
 
       resources :tokens, only: [:create]
 
-      resources :products, only: %i[show index]
+      resources :products
+
 
     end
   end
